6414b76eca14acc8cc651d7f83e81b0434369b2f
Jeffry Gonzalez <jeff@hypertheory.com>
2024-10-18 09:22:04 -0400

    some notes

--- practices/StringCalculatorKata/StringCalculator/Calculator.cs
+++ practices/StringCalculatorKata/StringCalculator/Calculator.cs
@@ -4,12 +4,19 @@ public class Calculator
 {
     public int Add(string numbers)
     {
+        // there was no requirement to check for nulls, and it's ok that you added it, but 
+        // you should have a test where you pass in a null to make sure this works.
+
         if (numbers == null || numbers.Length == 0)
         {
             return 0;
         }
         else if (numbers.Length == 1)
         {
+            // This, I think, can be safely removed. I don't understand the special case
+            // for only single digit numbers. Why is "1" treated differently than "10"?
+            // "else if" is always a minor code smell. If you can avoid it, do it. I think
+            // you can.
             return convertToInteger(numbers);
         }
         else
@@ -36,8 +43,9 @@ public class Calculator
 
     private int convertToInteger(string number)
     {
-        int num = 0;
-        if (int.TryParse(number, out num))
+        // C# allows you to declare the out var inline
+        // int num = 0;
+        if (int.TryParse(number, out int num))
         {
             return num > 1000 ? 0 : num;
         }
@@ -47,18 +55,24 @@ public class Calculator
         }
     }
 
+    // i like how you abstracted this.
     private string[] getDelimiters(string numbers)
     {
         var delimiters = new List<string> { ",", "\n" };
         if (numbers.StartsWith("//"))
         {
+            // I'm not really getting what you are doing here, but looks good.
             if (numbers[2].Equals('['))
             {
+                // to learn something cool, hover over that regex and read the link
+                // suggesting a GeneratedRegexAttribute.
                 var Matches = Regex.Matches(numbers, @"\[(.*?)\]");
+                // consistency in var or not? minor. also "String" and "string" in C# are the same thing.
+                // it is more idiomatic to use the lower-case version, again, minor.
                 String customDelimiters = "";
-                foreach (Match Match in Matches)
+                foreach (Match match in Matches)
                 {
-                    customDelimiters += (Match.Groups[1].Value);
+                    customDelimiters += (match.Groups[1].Value);
                 }
                 String[] strs = customDelimiters.Split(',');
                 foreach (string str in strs)
