cb132e306d73bbd8d963f0bd6c1d19cfaa37ebf7
Kaung Myat Lwin <kgmyatlwin89@gmail.com>
2024-10-21 13:50:25 -0400

    Angular Day 1

--- week2/frontend/src/app/app.component.ts
+++ week2/frontend/src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from './components/welcome.component';
+import { NavigationComponent } from './components/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: `
+    <app-navigation />
     <main class="container mx-auto">
-      <app-welcome />
+      <router-outlet />
     </main>
   `,
   styles: [],
-  imports: [RouterOutlet, WelcomeComponent],
+  imports: [RouterOutlet, WelcomeComponent, NavigationComponent],
 })
 export class AppComponent {}
